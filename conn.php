11d42d20515fc8ffeb986d3bd5a3266d6f2c44b9
veera <veeraragavanbe@gmail.com>
2020-03-30 00:27:02 +0530

    Connection Establishin with Db

---
+++ conn.php
@@ -0,0 +1,11 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="blood_donate_system";
+
+// Create connection
+$conn =new  mysqli($servername, $username, $password,$dbname);
+
+?> 
+
